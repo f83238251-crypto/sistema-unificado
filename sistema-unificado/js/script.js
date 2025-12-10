7a1a21bd1c3c5b4ed2674289f9101164debc8d94
FREDY <fredy@babyshower.com>
2025-12-09 22:27:30 -0500

    Eliminar duplicados de la carpeta sistema-unificado

--- sistema-unificado/js/script.js
+++
@@ -1,8 +0,0 @@
-// Solo función para abrir enlaces externos en nueva pestaña
-window.openLink = function(url) {
-  if (url === "#") {
-    alert("Enlace no disponible.");
-    return;
-  }
-  window.open(url, '_blank');
-};
